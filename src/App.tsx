202c90d79f6222d679652ece9a184aaeff74c12c
Mateusz Bukowski <m.bukowski@commonwombat.pl>
2021-02-10 08:00:00 +0100

    mobx context deleted

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,12 @@ import { mainTheme } from './theme/mainTheme';
 import { ThemeProvider } from 'styled-components';
 import AppContainer from './components/AppContainer';
 import GlobalStyle from './theme/GlobalStyle';
-import RootStore from './stores/RootStore';
-import MobXContext from './stores/MobXContext';
 
 function App() {
   return (
     <ThemeProvider theme={mainTheme}>
-      <MobXContext.Provider value={RootStore}>
-        <AppContainer />
-        <GlobalStyle />
-      </MobXContext.Provider>
+      <AppContainer />
+      <GlobalStyle />
     </ThemeProvider>
   );
 }
